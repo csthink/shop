8c6860f79cd64545a17263656f73b71b9768e6cc
mydu <security.2009@live.cn>
2019-01-03 20:38:08 +0800

    添加基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,3 +5,10 @@
  * Date: 2018-12-26
  * Time: 15:08
  */
+
+// 将当前请求的路由名称转换为 CSS 类名称,允许我们针对某个页面做页面样式定制
+function route_class()
+{
+    return str_replace(',', '-', Route::currentRouteName());
+}
+
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', '首页')
+
+@section('content')
+    首页内容
+@stop
